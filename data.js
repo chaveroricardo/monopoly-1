0b975bfd82d195e458812f243b1887c1366b8225
chansecampbell <chansecampbell@gmail.com>
2016-07-07 15:54:07 +0100

    Added some properties

--- data.js
+++ data.js
@@ -10,4 +10,47 @@ window.data = [{
   rental: [4, 20, 60, 180, 360, 450],
   color: "brown",
   housePrice: 50
+},{
+  name: "Coventry Street",
+  price: 300,
+  rental: [22, 110, 330, 800, 975, 1150],
+  color: "yellow",
+  housePrice: 150
+},{
+  name: "Piccadilly",
+  price: 300,
+  rental: [22, 120, 360, 850, 1025, 1200],
+  color: "yellow",
+  housePrice: 140
+},{
+  name: "Regent Street",
+  price: 400,
+  rental: [26, 130, 390, 900, 1100, 1275],
+  color: "green",
+  housePrice: 150
+},{
+  name: "Oxford Street",
+  price: 400,
+  rental: [26, 130, 390, 900, 1100, 1275],
+  color: "green",
+  housePrice: 150
+},{
+  name: "Bond Street",
+  price: 400,
+  rental: [28, 150, 450, 1000, 1200, 1400]
+  color: "dark blue",
+  housePrice: 200
+},{
+  name: "Park Lane",
+  price: 350,
+  rental: [35, 175, 500, 1100, 1300, 1500],
+  color: "dark blue",
+  housePrice: 200
+},{
+  name: "Mayfair",
+  price: 400,
+  rental: [50, 200, 600, 1400, 1700, 2000],
+  color: "dark blue",
+  housePrice: 200
+}
 }]
