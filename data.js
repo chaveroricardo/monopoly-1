d54352b2cde8983cc6c70413b0061f3931ce9783
mickyginger <mickyginger@gmail.com>
2016-07-07 15:39:57 +0100

    Updated data file

---
+++ data.js
@@ -0,0 +1,13 @@
+window.data = [{
+  name: "Old Kent Road",
+  price: 60,
+  rental: [2, 10, 30, 90, 160, 250],
+  color: "brown",
+  housePrice: 50
+},{
+  name: "Whitechapel Road",
+  price: 60,
+  rental: [4, 20, 60, 180, 360, 450],
+  color: "brown",
+  housePrice: 50
+}]
