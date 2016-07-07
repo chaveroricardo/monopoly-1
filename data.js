e68ac6c73fcb8e50ad7f2098cf6a6e4036e7fb9e
chansecampbell <chansecampbell@gmail.com>
2016-07-07 16:06:23 +0100

    Finished adding data to all of the standard property types.

--- data.js
+++ data.js
@@ -11,48 +11,6 @@ window.data = [{
   color: "brown",
   housePrice: 50
 },{
-  name: "Coventry Street",
-  price: 300,
-  rental: [22, 110, 330, 800, 975, 1150],
-  color: "yellow",
-  housePrice: 150
-},{
-  name: "Piccadilly",
-  price: 300,
-  rental: [22, 120, 360, 850, 1025, 1200],
-  color: "yellow",
-  housePrice: 140
-},{
-  name: "Regent Street",
-  price: 400,
-  rental: [26, 130, 390, 900, 1100, 1275],
-  color: "green",
-  housePrice: 150
-},{
-  name: "Oxford Street",
-  price: 400,
-  rental: [26, 130, 390, 900, 1100, 1275],
-  color: "green",
-  housePrice: 150
-},{
-  name: "Bond Street",
-  price: 400,
-  rental: [28, 150, 450, 1000, 1200, 1400]
-  color: "dark blue",
-  housePrice: 200
-},{
-  name: "Park Lane",
-  price: 350,
-  rental: [35, 175, 500, 1100, 1300, 1500],
-  color: "dark blue",
-  housePrice: 200
-},{
-  name: "Mayfair",
-  price: 400,
-  rental: [50, 200, 600, 1400, 1700, 2000],
-  color: "dark blue",
-  housePrice: 200
-},{
   name: "The Angel, Islington",
   price: 100,
   rental: [6, 30, 90, 270, 400, 550],
@@ -106,4 +64,70 @@ window.data = [{
   rental: [16, 80, 220, 600, 800, 1000],
   color: "orange",
   housePrice: 100
+},{
+  name: "The Strand",
+  price: 220,
+  rental: [18, 90, 250, 700, 875, 1050],
+  color: "red",
+  housePrice: 150
+},{
+  name: "Fleet Street",
+  price: 220,
+  rental: [18, 90, 250, 700, 875, 1050],
+  color: "red",
+  housePrice: 150
+},{
+  name: "Trafalgar Square",
+  price: 240,
+  rental: [20, 100, 300, 750, 925, 1100],
+  color: "red",
+  housePrice: 150
+},{
+  name: "Leicester Square",
+  price: 300,
+  rental: [22, 110, 330, 800, 975, 1150],
+  color: "yellow",
+  housePrice: 150
+},{
+  name: "Coventry Street",
+  price: 300,
+  rental: [22, 110, 330, 800, 975, 1150],
+  color: "yellow",
+  housePrice: 150
+},{
+  name: "Piccadilly",
+  price: 300,
+  rental: [22, 120, 360, 850, 1025, 1200],
+  color: "yellow",
+  housePrice: 140
+},{
+  name: "Regent Street",
+  price: 400,
+  rental: [26, 130, 390, 900, 1100, 1275],
+  color: "green",
+  housePrice: 150
+},{
+  name: "Oxford Street",
+  price: 400,
+  rental: [26, 130, 390, 900, 1100, 1275],
+  color: "green",
+  housePrice: 150
+},{
+  name: "Bond Street",
+  price: 400,
+  rental: [28, 150, 450, 1000, 1200, 1400]
+  color: "dark blue",
+  housePrice: 200
+},{
+  name: "Park Lane",
+  price: 350,
+  rental: [35, 175, 500, 1100, 1300, 1500],
+  color: "dark blue",
+  housePrice: 200
+},{
+  name: "Mayfair",
+  price: 400,
+  rental: [50, 200, 600, 1400, 1700, 2000],
+  color: "dark blue",
+  housePrice: 200
 }]
