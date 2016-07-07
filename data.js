2030076057277c8561d769c78e5312187a54fa39
mickyginger <mickyginger@gmail.com>
2016-07-07 15:52:59 +0100

    First 2 sides of normal properties done

--- data.js
+++ data.js
@@ -10,4 +10,58 @@ window.data = [{
   rental: [4, 20, 60, 180, 360, 450],
   color: "brown",
   housePrice: 50
+},{
+  name: "The Angel, Islington",
+  price: 100,
+  rental: [6, 30, 90, 270, 400, 550],
+  color: "lightBlue",
+  housePrice: 50
+},{
+  name: "Euston Road",
+  price: 100,
+  rental: [6, 30, 90, 270, 400, 550],
+  color: "lightBlue",
+  housePrice: 50
+},{
+  name: "Pentonville Road",
+  price: 120,
+  rental: [8, 40, 100, 300, 450, 600],
+  color: "lightBlue",
+  housePrice: 50
+},{
+  name: "Pall Mall",
+  price: 140,
+  rental: [10, 50, 150, 450, 625, 750],
+  color: "pink",
+  housePrice: 100
+},{
+  name: "Whitehall",
+  price: 140,
+  rental: [10, 50, 150, 450, 625, 750],
+  color: "pink",
+  housePrice: 100
+},{
+  name: "Northumberland Avenue",
+  price: 160,
+  rental: [12, 60, 180, 500, 700, 900],
+  color: "pink",
+  housePrice: 100
+},{
+  name: "Bow Steet",
+  price: 180,
+  rental: [14, 70, 200, 550, 750, 950],
+  color: "orange",
+  housePrice: 100
+},{
+  name: "Marlborough Steet",
+  price: 180,
+  rental: [14, 70, 200, 550, 750, 950],
+  color: "orange",
+  housePrice: 100
+},{
+  name: "Vine Steet",
+  price: 200,
+  rental: [16, 80, 220, 600, 800, 1000],
+  color: "orange",
+  housePrice: 100
 }]
